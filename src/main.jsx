206e3890cf0e506195bf13151f6ea58d79273298
belaeat <omyhossain@gmail.com>
2023-05-04 16:06:26 +0600

    navbar working properly

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Main from './layout/Main.jsx';
+import Home from './components/Home/Home.jsx';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Blog from './components/Blog/Blog';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      },
+      {
+        path: 'register',
+        element: <Register></Register>
+      },
+      {
+        path: 'blog',
+        element: <Blog></Blog>
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
