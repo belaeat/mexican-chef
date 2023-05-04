8e428953d98b4a78d46bcf21717ae588113aba50
belaeat <omyhossain@gmail.com>
2023-05-05 02:02:31 +0600

    chefs data and recipes working fine

--- src/components/ChefsBanner/ChefsBanner.jsx
+++ src/components/ChefsBanner/ChefsBanner.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
 
 const ChefsBanner = ({ info }) => {
 
     const { name, imageUrl, rating, ingredients, method } = info;
-    // console.log(info.id)
-
-    // const chefsInfo = useLoaderData()
-    // console.log(chefsInfo)
 
     return (
         <div>
-            <div className="hero min-h-screen bg-base-200">
+            <div className="hero min-h-screen bg-yellow-200 my-12 rounded-lg text-black">
                 <div className="hero-content flex-col lg:flex-row">
-                    <img className='w-[50%]' src={imageUrl} />
-                    <div>
-                        <h1>{name}</h1>
-                        <p>Ingredients: {ingredients}</p>
-                        <p>Cooking Method: {method}</p>
-                        <p>Rating: {rating}</p>
-                        <button className="btn btn-primary my-6">Add To Favorite</button>
+                    <img className='w-[50%] h-full' src={imageUrl} />
+                    <div className='ms-6'>
+                        <h1 className='font-bold text-3xl mb-4'>{name}</h1>
+                        <p><span className='font-bold mb-4'>Ingredients:</span> {ingredients}</p>
+                        <p><span className='font-bold mb-4'>Cooking Method:</span> {method}</p>
+                        <p><span className='font-bold mb-4'>Rating:</span> {rating}</p>
+                        <button className="btn btn-primary my-6 bg-black text-yellow-200 outline-none hover:bg-white hover:text-black">Add To Favorite</button>
                     </div>
                 </div>
             </div>
--- src/components/ChefsPage/ChefsPage.jsx
+++ src/components/ChefsPage/ChefsPage.jsx
@@ -4,43 +4,25 @@ import ChefsBanner from '../ChefsBanner/ChefsBanner';
 
 const ChefsPage = () => {
     const { id } = useParams()
-    const [data, setData] = useState([])
     const chefsInfo = useLoaderData()
+    const checkData = chefsInfo.find(chefs => chefs.chef_id == id)
+    // console.log(checkData)
 
-    setData(chefsInfo)
-
-    // console.log(chefsInfo)
-
-
-    const [chef, setChef] = useState({})
-
-    useEffect(() => {
-        const checkData = data.find(chefs => chefs.chef_id == id)
-        setChef(checkData)
-    }, [data])
-
-    console.log(checkData)
-
-    // console.log(chef)
-
-    // useEffect(() => {
-    //     const checkData = chefsInfo.find(chefs => chefs.chef_id == id)
-    //     setChef(checkData);
-    // }, [chef_id])
 
     return (
         <div>
-            <div className='flex'>
-                <img className='w-[50%]' src={chef?.chef?.chefUrl} alt="" />
-                <div>
-                    <h1 className="text-5xl font-bold"></h1>
-                    <p className="py-6"></p>
-                    <p>Likes: </p>
-                    <p>Recipes: </p>
-                    <p> Years of experiences!</p>
+            <div className='flex items-center rounded-lg bg-white text-black my-12'>
+                <img className='w-[50%]' src={checkData.chef.chefUrl} alt="" />
+                <div className='ms-6'>
+                    <h1 className="text-5xl font-bold">{checkData.chef.name}</h1>
+                    <p className="py-6">{checkData.chef.bio}</p>
+                    <p><span className='font-bold'>Likes:</span> {checkData.chef.likes}</p>
+                    <p><span className='font-bold'>Recipes:</span> {checkData.chef.numRecipes}</p>
+                    <p className='font-bold'>{checkData.chef.yearsOfExperience} Years of experiences!</p>
                 </div>
             </div>
-            <div className='bg-yellow-200'>
+            <h2 className='font-bold text-white text-center text-3xl my-12'>{checkData.chef.name}'s Special Recipes</h2>
+            <div className=''>
                 {
                     chefsInfo.map(info => <ChefsBanner
                         key={info.id}
